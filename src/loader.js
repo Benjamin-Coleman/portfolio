5e97574420cd16771d34255387dd8863768f83b4
raroul <raoul.gaillard@hetic.net>
2017-03-18 01:43:57 +0100

    Update loader

--- src/loader.js
+++ src/loader.js
@@ -2,22 +2,18 @@ require('./commons/style/loader.scss')
 
 class Loader {
 	constructor() {
-		this.linesScale = .1
+		this.linesScale = 0
+		this.targetState = 100/3/100
 		this.$el = document.querySelector('.loader-container')
 		this.$loaderThinLine = document.querySelector('.loader__thin-line')
 		this.$loaderBigLine = document.querySelector('.loader__big-line')
+		this.$loaderState = document.querySelector('.loader__state')
 		this.startProgress()
 		this.events()
 	}
 
 	startProgress(){
-		this.linesScale += (1 - this.linesScale) * 0.003
-
-		this.$loaderThinLine.style.webkitTransform = "scaleX("+this.linesScale+")";
-		this.$loaderThinLine.style.MozTransform = "scaleX("+this.linesScale+")";
-		this.$loaderThinLine.style.msTransform = "scaleX("+this.linesScale+")";
-		this.$loaderThinLine.style.OTransform = "scaleX("+this.linesScale+")";
-		this.$loaderThinLine.style.transform = "scaleX("+this.linesScale+")";
+		this.linesScale += (this.targetState - this.linesScale) * 0.009
 
 		this.$loaderBigLine.style.webkitTransform = "scaleX("+this.linesScale+")";
 		this.$loaderBigLine.style.MozTransform = "scaleX("+this.linesScale+")";
@@ -25,15 +21,29 @@ class Loader {
 		this.$loaderBigLine.style.OTransform = "scaleX("+this.linesScale+")";
 		this.$loaderBigLine.style.transform = "scaleX("+this.linesScale+")";
 
-		this.linesScale >= 0.9 ? undefined : requestAnimationFrame(this.startProgress.bind(this))
+		this.parseLoadingState()
+		this.linesScale >= 	1 ? undefined : requestAnimationFrame(this.startProgress.bind(this))
 	}
 
 	events(){
-		window.addEventListener('load', this.pageReady.bind(this))
+		document.addEventListener('readystatechange', this.readyStateChange.bind(this, event))
+	}
+
+	readyStateChange(){
+		this.targetState += 100/2/100
+		document.readyState === 'complete' ? this.pageReady() : undefined
+	}
+
+	getLoadingState(){
+		return Math.floor(this.linesScale * 100)
+	}
+
+	parseLoadingState(){
+		this.$loaderState.innerText = this.getLoadingState() + '%'
 	}
 
 	pageReady(){
-		window.removeEventListener('load', this.pageReady.bind(this))
+		document.removeEventListener('readystatechange', this.pageReady.bind(this))
 		this.linesScale = 1
 		this.$loaderThinLine.classList.add('loader__line--active')
 		this.$loaderBigLine.classList.add('loader__line--active')
