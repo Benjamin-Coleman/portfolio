fc0bd991b3dc592610dda734e215d917c295020c
raroul <raoul.gaillard@hetic.net>
2017-04-05 18:46:34 +0200

    Add google analytucs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 
 import Router from 'vue-router'
@@ -9,6 +10,11 @@ Vue.use(Router)
 const router = new Router({
   routes: routes,
   mode: 'history',
+})
+
+Vue.use(VueAnalytics, {
+  id: 'UA-96857336-1',
+  router
 })
 
 const app = new Vue({
